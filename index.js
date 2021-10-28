a85f0966b6f4e483931a50f558b8f52254d3ca11
tanvinpavel <tanvinpavel123@gmail.com>
2021-10-28 11:40:46 +0600

    Port updated

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { MongoClient } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
